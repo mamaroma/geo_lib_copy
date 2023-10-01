9ae4341f5916d7588f8552588a5faa8ea8691515
profitrolinka <glebbond02@mail.ru>
2023-10-01 20:15:13 +0300

    commets added

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''получает на вход r, возвращает площадь круга с радиусом r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''получает на вход r, возвращает периметр круга с радиусом r'''
     return 2 * math.pi * r
 
